faa89b7618c7781702ee1ef9ba4a50af67cc6d3c
Rhuan <86356608+RHU4N@users.noreply.github.com>
2025-09-27 16:01:00 -0300

    revertendo

--- src/infrastructure/api/UserApiRepository.js
+++ src/infrastructure/api/UserApiRepository.js
@@ -4,12 +4,12 @@ import axios from 'axios';
 export default class UserApiRepository extends UserRepository {
 	async forgotPassword(email) {
 		const apiUrl = process.env.REACT_APP_API_LOGIN_URL;
-		await axios.post(`${apiUrl}/users/forgot-password`, { email });
+		await axios.post(`${apiUrl}/user/forgot-password`, { email });
 		return true;
 	}
 	async register(userData) {
 		const apiUrl = process.env.REACT_APP_API_LOGIN_URL;
-		const response = await axios.post(`${apiUrl}/users`, {
+		const response = await axios.post(`${apiUrl}/user`, {
 			nome: userData.nome,
 			email: userData.email,
 			senha: userData.senha,
@@ -22,9 +22,9 @@ export default class UserApiRepository extends UserRepository {
 
 	async login({ email, senha }) {
 		const apiUrl = process.env.REACT_APP_API_LOGIN_URL;
-		const res = await axios.post(`${apiUrl}/auth/login`, { email, senha });
+		const res = await axios.post(`${apiUrl}/user/login`, { email, senha });
 		const { token } = res.data;
-		const userRes = await axios.get(`${apiUrl}/auth`, {
+		const userRes = await axios.get(`${apiUrl}/user`, {
 			headers: { Authorization: `Bearer ${token}` }
 		});
 		const userData = userRes.data.find(u => u.email === email);
