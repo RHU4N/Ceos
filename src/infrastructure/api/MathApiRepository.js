70825febc1d4530dd58f9b5fbd8399caec862914
RHU4N <rhuan3003@gmail.com>
2025-10-11 10:20:33 -0300

    test local

--- src/infrastructure/api/MathApiRepository.js
+++ src/infrastructure/api/MathApiRepository.js
@@ -1,22 +1,20 @@
 // Implementação do MathRepository usando axios
 import MathRepository from '../../domain/repositories/MathRepository';
 import axios from 'axios';
+    const apiUrl = process.env.REACT_APP_API_MATH_URL || 'http://localhost:8080';
 
 export default class MathApiRepository extends MathRepository {
   async calcularFuncao(params) {
-    const apiUrl = process.env.REACT_APP_API_MATH_URL;
     let url = `${apiUrl}/funcao/${params.tipo}`;
     const res = await axios.post(url, params.data);
     return res.data.resultado;
   }
   async calcularEstatistica(params) {
-    const apiUrl = process.env.REACT_APP_API_MATH_URL;
     let url = `${apiUrl}/estatistica/${params.tipo}`;
     const res = await axios.post(url, params.data);
     return res.data.resultado;
   }
   async calcularAnaliseComb(params) {
-    const apiUrl = process.env.REACT_APP_API_MATH_URL;
     let url = `${apiUrl}/analise/${params.tipo}`;
     const res = await axios.post(url, params.data);
     return res.data.resultado;
